5b3b208ebe0db531672d200af45309ffe3c5020c
Lucas <lucasroquesousa@hotmail.com>
2022-09-11 11:45:41 -0300

    feat: finalizando componentes de input e text area

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import logoimg from "../../public/Group 180.png";
+import styles from "../../styles/Home.module.scss";
+import { Input } from "../components/ui/Input";
 
 const Home = () => {
   return (
@@ -8,8 +11,14 @@ const Home = () => {
       <Head>
         <title>Sênior - Faça seu login</title>
       </Head>
-      <div>
-        <h1>Sênior - Pizzaria e Pastelaria </h1>
+      <div className={styles.containerCenter}>
+        <Image src={logoimg} alt="Logo Pizzaria" />
+        <div className={styles.login}>
+          <form>
+            <Input type="text" placeholder="Digite seu email" />
+            <Input type="password" placeholder="Digite sua senha" />
+          </form>
+        </div>
       </div>
     </>
   );
