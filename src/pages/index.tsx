3954bc59849e4e0406ac2a6e60dda1f6b27fafe5
Lucas <lucasroquesousa@hotmail.com>
2022-09-11 10:29:11 -0300

    feat: finalizando configuração do globals styles

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Olá</h1>
+    </div>
+  );
+};
+
+export default Home;
